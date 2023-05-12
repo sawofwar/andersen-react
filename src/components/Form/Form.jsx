4b2581970f94e3f67ff425a67433088454c7d698
ConstantineF <warsawxx@gmail.com>
2023-05-12 17:54:34 +0400

    add form

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+// import PropTypes from "prop-types";
+
+import "./Form.css";
+
+class Form extends React.Component {
+  // constructor(props) {
+  // super(props);
+  // this.props = props;
+  // }
+
+  render() {
+    return <></>;
+  }
+}
+
+// Form.propTypes = {
+// placeholder: PropTypes.string,
+// };
+
+export default Form;
