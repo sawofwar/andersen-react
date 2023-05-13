9f53d991995cc0d1bc532a54aab8715d12f0b9a7
ConstantineF <warsawxx@gmail.com>
2023-05-13 17:28:01 +0400

    prevent default submit form action

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,9 +4,17 @@ import PropTypes from "prop-types";
 import "./Form.css";
 
 class Form extends React.Component {
+  formSubmitHandler(e) {
+    e.preventDefault();
+  }
+
   render() {
     return (
-      <form className="form-card" action="submit">
+      <form
+        className="form-card"
+        action="submit"
+        onSubmit={this.formSubmitHandler.bind(this)}
+      >
         {this.props.children}
       </form>
     );
