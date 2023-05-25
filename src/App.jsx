67688587d38a62e8e01df59e55ea12f79684f11c
ConstantineF <warsawxx@gmail.com>
2023-05-25 05:32:47 +0400

    add new reducer to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,31 @@ import "./App.css";
 import React from "react";
 import BigForm from "./components/BigForm/BigForm";
 
+const initialState = {
+  name: "",
+  surname: "",
+  birthday: "",
+  phone: "",
+  site: "",
+  description: "",
+  stack: "",
+  project: "",
+};
+
+const reducer = (state, action, value) => {
+  console.log("hi from app reducer");
+};
+
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = initialState;
+  }
+
+  dispatch = (action) => {
+    this.setState((prevState) => reducer(prevState, action));
+  };
+
   bigFormCancelHandler() {
     console.log("");
   }
