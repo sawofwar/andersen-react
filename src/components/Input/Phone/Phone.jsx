af83619de0b9f10f85ef1ab1fa1aa7677d7d255c
ConstantineF <warsawxx@gmail.com>
2023-05-27 00:18:38 +0400

    make Name.jsx & Phone.jsx functional

--- src/components/Input/Phone/Phone.jsx
+++ src/components/Input/Phone/Phone.jsx
@@ -1,14 +1,14 @@
-import Input from "../Input";
+import PropTypes from "prop-types";
 
 import { checkPhone } from "../../../utils/CheckPhone";
 
 import "./../Input.css";
-import { createRef } from "react";
+import { useRef, useState } from "react";
 
 import actionTypes from "../../../utils/ActionTypes";
 import appActionTypes from "../../../utils/AppActionTypes";
 
-class Phone extends Input {
+/* class Phone extends Input {
   constructor(props) {
     super(props);
 
@@ -119,6 +119,123 @@ class Phone extends Input {
       </div>
     );
   }
-}
+} */
+
+/* this.state = {
+      isWarned: false,
+      avoidHyphen: false,
+    };
+
+    this.warningRef = createRef(); */
+
+const Phone = ({
+  id,
+  label,
+  placeholder,
+  forwardedRef,
+  dispatch,
+  appDispatch,
+}) => {
+  const [isWarned, setIsWarned] = useState();
+  const [avoidHyphen, setAvoidHyphen] = useState();
+  const warningRef = useRef();
+
+  const inputChangeHandler = (event) => {
+    const target = event.target;
+    const targetValue = event.target.value;
+
+    if (
+      (targetValue.at(-1) === "-" && targetValue.length !== 2) ||
+      (targetValue.at(-1) === "-" && targetValue.length !== 7) ||
+      (targetValue.at(-1) === "-" && targetValue.length !== 10)
+    ) {
+      const targetValueArray = targetValue.split("");
+      targetValueArray.pop();
+      target.value = targetValueArray.join("");
+    }
+
+    if (
+      event.target.value.length === 1 &&
+      event.target.value !== "-" &&
+      avoidHyphen === false
+    ) {
+      target.value = target.value + "-";
+    } else if (event.target.value.length === 6 && avoidHyphen === false) {
+      target.value = target.value + "-";
+    } else if (event.target.value.length === 9 && avoidHyphen === false) {
+      target.value = target.value + "-";
+    }
+
+    const isValid = checkPhone(event.target.value);
+
+    if (isValid === "empty") {
+      setIsWarned(false);
+      event.target.style.outline = "none";
+
+      dispatch({ type: actionTypes.PHONE_FALSE });
+      return;
+    }
+
+    if (!isValid) {
+      setIsWarned(true);
+      dispatch({ type: actionTypes.PHONE_FALSE });
+
+      event.target.style.outline = "var(--input-warning-outline)";
+      const warningRefCurrent = warningRef?.current ?? { textContent: "" };
+      warningRefCurrent.textContent = "Формат: 7-7777-77-77";
+    } else {
+      setIsWarned(false);
+      dispatch({ type: actionTypes.PHONE_TRUE });
+      appDispatch({ type: appActionTypes.PHONE_ALTER }, event.target.value);
+
+      event.target.style.outline = "none";
+    }
+  };
+
+  const keyDownHandler = (event) => {
+    const target = event.target;
+
+    if (event.code === "Backspace") {
+      setAvoidHyphen(true);
+      target.length = target.length - 1;
+
+      setTimeout(() => {
+        setAvoidHyphen(false);
+      }, 10);
+    }
+  };
+
+  return (
+    <div className="input-block">
+      <label className="input-label" htmlFor={`${id}-input`}>
+        {label}
+      </label>
+
+      <input
+        onChange={inputChangeHandler}
+        placeholder={placeholder}
+        type="string"
+        className="input-input"
+        id={`${id}-input`}
+        maxLength={12}
+        ref={forwardedRef}
+        onKeyDown={keyDownHandler}
+      />
+      {isWarned && (
+        <p className="input-warning" ref={warningRef}>
+          Формат: 7-7777-77-77
+        </p>
+      )}
+    </div>
+  );
+};
+
+Phone.propTypes = {
+  placeholder: PropTypes.string,
+  label: PropTypes.string,
+  id: PropTypes.string,
+  dispatch: PropTypes.func,
+  appDispatch: PropTypes.func,
+};
 
 export default Phone;
